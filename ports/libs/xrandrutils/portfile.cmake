eed6624b8f860fb14dc9073d5f7c7e089c34c936
Alexander Neumann <Alexander.Neumann@hamburg.de>
2022-01-17 23:54:28 +0100

    xrandrutils

---
+++ ports/libs/xrandrutils/portfile.cmake
@@ -0,0 +1,37 @@
+if(NOT X_VCPKG_FORCE_VCPKG_X_LIBRARIES AND NOT VCPKG_TARGET_IS_WINDOWS)
+    message(STATUS "Utils and libraries provided by '${PORT}' should be provided by your system! Install the required packages or force vcpkg libraries by setting X_VCPKG_FORCE_VCPKG_X_LIBRARIES")
+    set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+else()
+if(VCPKG_TARGET_IS_WINDOWS)
+    vcpkg_check_linkage(ONLY_STATIC_LIBRARY) # Build should generate an import lib but somehow it is not created and thus it fails the install rule. 
+endif()
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.freedesktop.org/xorg
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lib/libxrandrutils
+    REF cd31e7fe468b7264181c41c5f62ed15de815dc7c 
+    SHA512  39622c14e8e25c3038805874d5e6686f4464f4f8656f7b8b7fce4180c1fd4bd41018e2bfe0102662e13b037db6fd6ba3f47136424a012bebbdba0d915f5a315b
+    HEAD_REF master # branch name
+    PATCHES fix_strings_h.patch
+) 
+
+set(ENV{ACLOCAL} "aclocal -I \"${CURRENT_INSTALLED_DIR}/share/xorg/aclocal/\"")
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+# # Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static OR NOT VCPKG_TARGET_IS_WINDOWS)
+        file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+endif()
